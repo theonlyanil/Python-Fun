1bce87b62452f262dc1c8994aa6582376b481850
theonlyanil <theonlyanil@gmail.com>
2020-09-17 00:45:39 +0530

    added comment

--- PD/pd0.py
+++ PD/pd0.py
@@ -6,20 +6,20 @@ import numpy as np
 symbols = ['AAPL', 'INFY', 'TCS', 'REL', 'JSL', 'JSW', 'AAPL', 'TCS', 'JSW', 'AAPL']
 type = ['BUY', 'SELL', 'SELL', 'BUY', 'BUY', 'SELL', 'BUY', 'BUY', 'BUY', 'SELL']
 amt = [100, 190, 200, 110, 120, 170, 300, 210, 190, 310]
+qty = [10, 8, 6, 3, 2, 8, 26, 9, 9, 28]
 data = {'Symbol':symbols,
         'Type': type,
-        'Value': amt
+        'Value': amt,
+        'Qty': qty
         }
 
 # Create DataFrame
 df = pd.DataFrame(data)
-print('\n\n\n')
-#df = df.groupby(["Symbol", "Type"]).agg({"Value": 'sum'}).reset_index()
-df = df.groupby(["Symbol", "Type"]).agg({"Value": 'sum'}).reset_index()
 
+#The below code tells that if type column is buy, multiply value column by 1 else by -1
 df['Value'] *= np.where(df['Type']=='BUY', 1, -1)
-df = df.groupby('Symbol', as_index=False).agg({'Type':'first','Value':'sum'})
+df['Qty'] *= np.where(df['Type']=='BUY', 1, -1)
+df = df.groupby('Symbol').agg({'Value':'sum', "Qty": 'sum'})
 
 # Print the output.
 print(df)
-print('\n\n\n')
